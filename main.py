766f0a0a8cadc3b4f4e96d4f76beee612de52703
rodney22@live.nl <rodney22@live.nl>
2022-04-09 16:35:43 +0200

    set ssl to off mode

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5
 database_name = os.environ.get('database_name', 'fastapi')
 db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
 db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
-ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
+ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','off')))
 DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 
 database = databases.Database(DATABASE_URL)
